129761b619f7e3fdf1dc2db0b3649455ab518d36
Teyo <131766045+Teyo1@users.noreply.github.com>
2024-07-20 23:42:51 +0300

    Update base3_to_decimal.py

--- Scripts/Decoding/base3_to_decimal.py
+++ Scripts/Decoding/base3_to_decimal.py
@@ -1,6 +1,6 @@
 def base3_to_decimal(base3_str):
     return int(base3_str, 3)
 
-number_base3 = "2010"
+number_base3 = "xxxx"
 decimal_number = base3_to_decimal(number_base3)
 print(decimal_number)  # Output: 57
